eca695bf6891211555960bef6d5c4bc5e4f1298a
alex210501 <alejandrobrb210501@gmail.com>
2022-02-15 20:52:11 +0100

    Add Controller class

---
+++ GigaBike/GigaBike/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaBike {
+    class Controller {
+        public Login Login { get; }
+        public Catalog Catalog { get; }
+        public Order Order { get; }
+        public DataBase DataBase { get; }
+
+        public Controller() {
+            this.Login = new Login();
+            this.Catalog = new Catalog();
+            this.Order = new Order();
+            this.DataBase = new DataBase();
+        }
+
+        public void Init() {
+            // Make the init sequence
+        }
+
+        public void AddToOrder(Bike bike, int quantity) {
+            // Add to order sequence
+        }
+    }
+}
