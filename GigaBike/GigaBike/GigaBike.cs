5571e6226bcc2922a3bc5dafe46d0860e0598135
alex210501 <alejandrobrb210501@gmail.com>
2022-02-15 20:55:19 +0100

    Add GogaBike class

---
+++ GigaBike/GigaBike/GigaBike.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaBike {
+    public class GigaBike {
+        Controller Controller;
+        // Display GigaBikeWindow;
+
+        GigaBike() {
+            Controller = new Controller();
+        }
+
+        void Initialize() {
+
+        }
+
+        void GoToLoginWindow() {
+
+        }
+
+        void GoToChooseUseWindow() {
+
+        }
+
+        void GoToCatalogWindow() {
+
+        }
+
+        void GoToBikeModelWindow(Bike bike) {
+
+        }
+
+        void GoToOrderWindow(Order order) {
+
+        }
+    }
+}
