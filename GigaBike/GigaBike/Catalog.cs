bbe1b4a705e75978c98e6c7a6e8848598f3678d9
alex210501 <alejandrobrb210501@gmail.com>
2022-02-15 20:48:19 +0100

    Add Catalog class

---
+++ GigaBike/GigaBike/Catalog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaBike {
+    class Catalog {
+        private List<Bike> bikes;
+
+        Catalog() {
+            bikes = new List<Bike>();
+        }
+
+        List<Bike> Bikes {
+            get {
+                return new List<Bike>(bikes);
+            }
+        }
+
+        void RefreshModels () { 
+            // Refresh the models from the database
+        }
+    }
+}
