54798dc5599f523a9595b3a08c367402a9e81d78
alex210501 <alejandrobrb210501@gmail.com>
2022-02-15 20:35:51 +0100

    Add Login class

---
+++ GigaBike/GigaBike/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaBike {
+    class Login {
+        private string Username { get; set; }
+        private string Password { get; set; }
+
+        public bool CheckUser(string username, string password) {
+            // Faire une requête en DB ici pour obtenir le mot de passe
+            // et vérifier sa correspondance avec le username
+            // Si le username n'est pas en DB, renvoie false
+            return true;
+        }
+    }
+}
