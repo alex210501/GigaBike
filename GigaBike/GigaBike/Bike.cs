0434ba6df3c3c3c8c205169c00e8a71a99a9cb15
alex210501 <alejandrobrb210501@gmail.com>
2022-02-15 20:37:54 +0100

    Add Bike class

---
+++ GigaBike/GigaBike/Bike.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaBike {
+    class Bike {
+        public string Name { get; }
+        public int Price { get; }
+        public string Color { get; }
+        public string Size { get; }
+
+        Bike (string name, int price, string color, string size) {
+            this.Name = name;
+            this.Price = price;
+            this.Color = color;
+            this.Size = size;
+        }
+    }
+}
