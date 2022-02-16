5228f11b5a0798e212e41a2f19cc1ac07241e1fb
alex210501 <alejandrobrb210501@gmail.com>
2022-02-16 23:12:04 +0100

    Add Color and Size class

---
+++ GigaBike/GigaBike/Color.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaBike {
+    public class Color {
+        public int IdColor { get; }
+        public string Name { get; }
+
+        public Color(int IdColor, string Name) {
+            this.IdColor = IdColor;
+            this.Name = Name;
+        }
+    }
+}
---
+++ GigaBike/GigaBike/Size.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaBike {
+    public class Size {
+        public int IdSize { get; }
+        public string Name { get; }
+
+        public Size(int IdSize, string Name) {
+            this.IdSize = IdSize;
+            this.Name = Name;
+        }
+    }
+}
