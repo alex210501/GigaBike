5a12720f4a8718e9a076f0cea538986a988bf37d
yiming <68252862+zhao-yiming@users.noreply.github.com>
2022-02-17 02:25:58 +0100

    complete methodes

--- GigaBike/GigaBike/DataBase.cs
+++ GigaBike/GigaBike/DataBase.cs
@@ -23,32 +23,32 @@ namespace GigaBike {
         public void Connect() {
             // Code to connect to the database
             // A voir, j'ai pas tester
-            this.connection = new MySqlConnection(string.Format(@"server={0};userid={1};pwd={2};persistsecurityinfo=True;database=dvauto;port={3};sharedmemoryname=", host, username, password, port));
+            this.connection = new MySqlConnection(string.Format(@"server={0};user id={1};password={2};persistsecurityinfo=True;database=GigaBike;port={3};sharedmemoryname=", host, username, password, port));
         }
 
         public MySqlDataReader GetPassword(string username) {
             // Tape la commande pour le mot de passe ici à l'intérieur
-            MySqlCommand command = SendCommand("") ;
+            MySqlCommand command = SendCommand("SELECT Password FROM Login WHERE UserName =" + username) ;
             return command.ExecuteReader();
         }
 
         public MySqlDataReader GetUniqueModel(int modelId) {
-            MySqlCommand command = SendCommand("");
+            MySqlCommand command = SendCommand("SELECT * FROM BikeModel WHERE IdModel =" + modelId);
             return command.ExecuteReader();
         }
 
         public MySqlDataReader GetModels() {
-            MySqlCommand command = SendCommand("");
+            MySqlCommand command = SendCommand("SELECT BikeModel.IdBike, BikeModel.IdModel, Bike.NameBike, BikeModel.IdColor, Color.NameColor, BikeModel.IdSize, Size.NameSize, BikeModel.Price FROM BikeModel INNER JOIN Bike ON Bike.IdBike = BikeModel.IdBike INNER JOIN Color ON Color.IdColor = BikeModel.IdColor INNER JOIN Size ON Size.IdSize = BikeModel.IdSize");
             return command.ExecuteReader();
         }
 
         public MySqlDataReader GetOrders() {
-            MySqlCommand command = SendCommand("");
+            MySqlCommand command = SendCommand("SELECT OrderInfo.IdOrder, OrderModel.IdModelBike, OrderInfo.DeliveryDate,Customer.NameCustomer, Customer.Address, Customer.TVA, Customer.Phone FROM OrderInfo INNER JOIN Customer ON Customer.TVA = OrderInfo.TvaCustomer INNER JOIN OrderModel ON OrderModel.IdOrder = OrderInfo.IdOrder");
             return command.ExecuteReader();
         }
 
         public MySqlDataReader GetCustomer(int customerId) {
-            MySqlCommand command = SendCommand("");
+            MySqlCommand command = SendCommand("SELECT * FROM Customer WHERE IdCustomer ="+ customerId);
             return command.ExecuteReader();
         }
 
