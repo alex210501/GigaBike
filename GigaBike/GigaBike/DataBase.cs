4ca26f008c5d60415b34a0f278a490be43474d84
Yiming Zhao <195187@ecam.be>
2022-02-18 03:59:41 +0100

    compete methodes and add "" with spaces

--- GigaBike/GigaBike/DataBase.cs
+++ GigaBike/GigaBike/DataBase.cs
@@ -28,7 +28,7 @@ namespace GigaBike {
 
         public MySqlDataReader GetPassword(string username) {
             // Tape la commande pour le mot de passe ici à l'intérieur
-            MySqlCommand command = SendCommand("SELECT Password FROM Login WHERE UserName =" + username) ;
+            MySqlCommand command = SendCommand('SELECT Password FROM Login WHERE UserName ="' + username+'"') ;
             return command.ExecuteReader();
         }
 
@@ -38,12 +38,17 @@ namespace GigaBike {
         }
 
         public MySqlDataReader GetModels() {
-            MySqlCommand command = SendCommand("SELECT BikeModel.IdBike, BikeModel.IdModel, Bike.NameBike, BikeModel.IdColor, Color.NameColor, BikeModel.IdSize, Size.NameSize, BikeModel.Price FROM BikeModel INNER JOIN Bike ON Bike.IdBike = BikeModel.IdBike INNER JOIN Color ON Color.IdColor = BikeModel.IdColor INNER JOIN Size ON Size.IdSize = BikeModel.IdSize");
+            MySqlCommand command = SendCommand("SELECT BikeModel.IdBike, BikeModel.IdModel, Bike.NameBike, BikeModel.IdColor, Color.NameColor, BikeModel.IdSize, Size.NameSize, BikeModel.Price FROM BikeModel " +
+                                                "INNER JOIN Bike ON Bike.IdBike = BikeModel.IdBike " +
+                                                "INNER JOIN Color ON Color.IdColor = BikeModel.IdColor " +
+                                                "INNER JOIN Size ON Size.IdSize = BikeModel.IdSize");
             return command.ExecuteReader();
         }
 
         public MySqlDataReader GetOrders() {
-            MySqlCommand command = SendCommand("SELECT OrderInfo.IdOrder, OrderModel.IdModelBike, OrderInfo.DeliveryDate,Customer.NameCustomer, Customer.Address, Customer.TVA, Customer.Phone FROM OrderInfo INNER JOIN Customer ON Customer.TVA = OrderInfo.TvaCustomer INNER JOIN OrderModel ON OrderModel.IdOrder = OrderInfo.IdOrder");
+            MySqlCommand command = SendCommand("SELECT OrderInfo.IdOrder, OrderModel.IdModelBike, OrderInfo.DeliveryDate,Customer.NameCustomer, Customer.Address, Customer.TVA, Customer.Phone FROM OrderInfo " +
+                                               "INNER JOIN Customer ON Customer.TVA = OrderInfo.TvaCustomer " +
+                                               "INNER JOIN OrderModel ON OrderModel.IdOrder = OrderInfo.IdOrder");
             return command.ExecuteReader();
         }
 
@@ -53,17 +58,17 @@ namespace GigaBike {
         }
 
         public MySqlDataReader GetStock() {
-            MySqlCommand command = SendCommand("");
+            MySqlCommand command = SendCommand("SELECT * FROM Part");
             return command.ExecuteReader();
         }
 
         public MySqlDataReader SetCustomer(Customer customer) {
-            MySqlCommand command = SendCommand("");
+            MySqlCommand command = SendCommand("INSERT INTO Customer VALUES ({0},{1},{2},{3})",customer.TVA,'"'+customer.NameCustomer+'"','"'+customer.Address+'"',customer.Phone);
             return command.ExecuteReader();
         }
 
         public MySqlDataReader SaveCommand(Order order) {
-            MySqlCommand command = SendCommand("");
+            MySqlCommand command = SendCommand("INSERT INTO OrderInfo VALUES (Null,{0},{1},{2},{3})",order.TvaCustomer,"'"+order.DeliveryDate+"'","'"+order.Duration+"'",order.Price);
             return command.ExecuteReader();
         }
     }
