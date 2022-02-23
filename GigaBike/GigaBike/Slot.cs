c9b0995e37dadae5b89788d5a9745784fc17178b
alex210501 <alejandrobrb210501@gmail.com>
2022-02-23 21:17:55 +0100

    Add WeekDay class

---
+++ GigaBike/GigaBike/Slot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaBike {
+
+    enum StateSlot {
+        BUSY = 0,
+        FREE = 1
+    }
+
+    class Slot {
+        public int SlotNumber { get; }
+        public StateSlot StateSlot { get; private set; }
+        public int IdOrder { get; set; }
+        public int IdOrderModel { get; set; }
+        public DateTime Date { get; private set; }
+
+        public Slot(int slotNumber) {
+            this.SlotNumber = slotNumber;
+            StateSlot = StateSlot.FREE;
+        }
+
+        public void BindSlotWithOrder(int idOrder, int idOrderModel) {
+            IdOrder = idOrder;
+            IdOrderModel = idOrderModel;
+        }
+    }
+}
---
+++ GigaBike/GigaBike/WeekDay.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaBike {
+    class WeekDay {
+        private readonly int SlotPerDay = 5;
+        private List<Slot> slots;
+        public DateTime Date { get; set; }
+        public int DayOfWeek { get; }
+
+        public WeekDay(int dayOfWeek) {
+            this.DayOfWeek = dayOfWeek;
+            slots = new List<Slot>(SlotPerDay);
+            Date = new DateTime();
+        }
+
+        public bool IsThereFreeSlots(int duration) {
+            return false;
+        }
+
+        public List<Slot> GetFreeSlots(int duration) {
+            return new List<Slot>();
+        }
+
+        public List<Slot> Slots {
+            get {
+                return new List<Slot>(slots);
+            }
+        }
+    }
+}
