9ad86550a0a6e7a63b6b8d6f875407f12fa3a754
alex210501 <alejandrobrb210501@gmail.com>
2022-02-23 21:46:24 +0100

    Add the Planning class

---
+++ GigaBike/GigaBike/Planning.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaBike {
+    class Planning {
+        private List<Week> weeks;
+
+        public Planning() {
+            weeks = new List<Week>();
+        }
+
+        public void Refresh() {
+            // Get information from the databse
+        }
+
+        public DateTime GetDeliveryDate(BikeOrder bikeOrder) {
+            return new DateTime();
+        }
+
+        public List<Slot> GetSlots(int duration) {
+            return new List<Slot>();
+        }
+
+        public List<Week> Weeks {
+            get {
+                return new List<Week>(weeks);
+            }
+        }
+    }
+}
