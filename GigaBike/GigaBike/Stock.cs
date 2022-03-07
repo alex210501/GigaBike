e651fa90a7689c6c96fa036b293a782d1535851b
Yiming Zhao <195187@ecam.be>
2022-03-07 17:44:13 +0100

    create class "Stcok"

---
+++ GigaBike/GigaBike/Stock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaBike
+{
+    public class Stock
+    {
+        public int IdStock { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public int Threshold { get; set; }
+        public int Location { get; set; }
+
+        public Stock() { }
+
+        public Stock(Stock otherStock)
+        {
+            this.IdStock = otherStock.IdStock;
+            this.Name = otherStock.Name;
+            this.Number = otherStock.Number;
+            this.Threshold = otherStock.Threshold;
+            this.Location = otherStock.Location;
+        }
+    }
+}
