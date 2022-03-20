3da1abd29d3d704d4c83af099ce35ecb824b1d48
alex210501 <alejandrobrb210501@gmail.com>
2022-03-20 14:42:41 +0100

    Add the PartToModelLinker Class

---
+++ GigaBike/GigaBike/PartToModelLinker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaBike {
+    class PartToModelLinker {
+        private Dictionary<int, List<Part>> partsPerBike;
+        public PartToModelLinker() {
+            partsPerBike = new Dictionary<int, List<Part>>();
+        }
+
+        public List<Part> GetPartsForIdOrderModel(int idOrderModel) {
+            if (partsPerBike.ContainsKey(idOrderModel))
+                return new List<Part>(partsPerBike[idOrderModel]);
+            return new List<Part>();
+        }
+
+        public void AddPartForIdOrderModel(int idOrderModel, Part part) {
+            // If the dictionnary doesn't contain the key, create an empty list
+            if (!partsPerBike.ContainsKey(idOrderModel))
+                partsPerBike.Add(idOrderModel, new List<Part>());
+            partsPerBike[idOrderModel].Add(part);
+        }
+
+        public void ClearParts() {
+            // Remove all the dictionnary items
+            partsPerBike.Clear();
+        }
+
+        public void ClearPartsForIdOrderModel(int idOrderModel) {
+            // Clear the part for only a bike model
+            if (partsPerBike.ContainsKey(idOrderModel))
+                partsPerBike[idOrderModel].Clear();
+        }
+    }
+}
