4034fef67837fe3399538f9ae7595e9c5a67dd72
mezray <18051@ecam.be>
2022-03-22 16:36:50 +0100

    Add page Order Part

---
+++ GigaBike/GigaBike/GUI/OrderPartPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GigaBike
+{
+    /// <summary>
+    /// Logique d'interaction pour OrderPartPage.xaml
+    /// </summary>
+    public partial class OrderPartPage : Page
+    {
+        public OrderPartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCreateNewPurchases(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonOrderPart(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
